365fbc9e8572f137a875fd17895575359d318bee
toantran00 <toan0974102841@gmail.com>
2025-09-16 10:18:42 +0700

    insert

--- replaceNonCoprimes.cpp
+++ replaceNonCoprimes.cpp
@@ -5,25 +5,26 @@ long long lcm(long long a,long long b)
     return a/gcd(a,b)*b;
 }
 vector<int> replaceNonCoprimes(vector<int>& nums) {
-       vector<int> result;
-    for (int num : nums) {
+    vector<int> result;
+    for(int num : nums)
+    {
         result.push_back(num);
-        while (result.size() >= 2) {
-            int x = result.back(); result.pop_back();
-            int y = result.back(); result.pop_back();
-            
-            int g = gcd(x, y);
-            if (g == 1) {
+        while (result.size()>1)
+        {
+            int x = result.back();
+            result.pop_back();
+            int y = result.back();
+            result.pop_back();
+            if(gcd(x,y) == 1)
+            {
                 result.push_back(y);
                 result.push_back(x);
                 break;
-            } else {
-                long long lcm_val = (long long)x / g * y;
-                result.push_back(lcm_val);
             }
+            long long lcm_x_y = lcm(x,y);
+            result.push_back(lcm_x_y);
         }
     }
-    
     return result;
     
 }
